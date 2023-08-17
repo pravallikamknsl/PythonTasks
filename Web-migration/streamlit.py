57db87aacf5c6510f215365786d0c9766658d7ff
Pravallika Motupalli <114479124+pravallikamknsl@users.noreply.github.com>
2023-08-17 12:28:53 +0530

    Update streamlit.py

--- Web-migration/streamlit.py
+++ Web-migration/streamlit.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+
+def main():
+    st.title("Streamlit App in Flask")
+
+# Your Streamlit code here
+    st.write("Hello from Streamlit!")
+
+
+if __name__ == '__main__':
+     main()
